020c8fc61b8415f7e4b44fd8fc6c30b389a1547f
tamara-f <filatovatam@ukr.net>
2020-04-14 18:20:45 +0300

    added js 3 tasks

---
+++ module-1/js/task_2.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const total = 100;
+let ordered;
+
+ let message_1 =  'На складе недостаточно твоаров!';
+
+ let message_2 =  'Заказ оформлен, с вами свяжется менеджер';
+
+ordered = 50;
+
+
+if  (ordered <= total) {
+    console.log(message_2);
+}
+else  {
+    console.log(message_1);}
+
+
+// new order
+
+ordered = 130;
+
+if  (ordered <= total) {
+    console.log(message_2);
+}
+else  {
+    console.log(message_1);}
+    
---
+++ module-1/js/task_3.js
@@ -0,0 +1,25 @@
+'use strict';
+
+ const ADMIN_PASSWORD = 'jqueryismyjam';
+let message_1 = 'Отменено пользователем!';
+let message_2 = 'Добро пожаловать!';
+let message_3 = 'Доступ запрещен, неверный пароль!';
+
+let password = prompt('Please enter password');
+console.log(password);
+
+
+switch (password) {
+
+    case null:
+        console.log(message_1);
+        break;
+
+    case ADMIN_PASSWORD:
+        alert(message_2);
+        break;
+
+    default:
+        alert(message_3);
+}
+
