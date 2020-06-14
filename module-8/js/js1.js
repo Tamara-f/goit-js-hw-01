8cf3dce1e1900daee935b3dcab94ccfdd57267a8
tamara-f <filatovatam@ukr.net>
2020-06-14 21:31:58 +0300

    module8 arrows

--- module-8/js/js1.js
+++ module-8/js/js1.js
@@ -1,4 +1,12 @@
-import galleryItems from '../js/gallery-items.js'; // eslint-disable-line
+import galleryItem from '../js/gallery-items.js'; // eslint-disable-line
+
+let __index;
+let counterValue = 0;
+
+const galleryItems = galleryItem.map(function (elem, index) {
+  elem.dataIndex = index + 1;
+  return elem;
+});
 
 const gallery = document.querySelector('.js-gallery');
 const lightbox = document.querySelector('.js-lightbox');
@@ -11,42 +19,90 @@ const createGallery = galleryItems.map(elem => {
   list.classList.add('gallery__item');
   const url = document.createElement('a');
   url.classList.add('gallery__link');
-  //   url.href = elem.original;
+  url.href = elem.original;
+  url.dataset.index = elem.dataIndex;
   const img = document.createElement('img');
   img.classList.add('gallery__image');
   img.src = elem.preview;
   img.dataset.source = elem.original;
+  img.dataset.index = elem.dataIndex;
   img.alt = elem.description;
   list.append(url);
   url.append(img);
   return list;
 });
+
 gallery.append(...createGallery);
 
 gallery.addEventListener('click', onTagsClick);
+
 function onTagsClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  __index = event.target.dataset.index;
   lightbox.classList.add('is-open');
   img.src = event.target.dataset.source;
-  window.addEventListener('keydown', onPressEsc);
+  window.addEventListener('keydown', onPressKeydown);
 }
 
-closeBtn.addEventListener('click', closeModal);
-
+closeBtn.addEventListener('click', closeModal); //кнопка закрыть
 backDrop.addEventListener('click', event => {
+  //серый фон
   if (event.target === event.currentTarget) {
     closeModal();
   }
 });
-function onPressEsc(event) {
+
+const url = function (nextUrl) {
+  //подстановка новой ссылки
+  img.src = findNextItemHref(
+    galleryItems,
+    nextUrl + Number(event.target.dataset.index)
+  );
+};
+const findNextItemHref = function (arr, index) {
+  __index = index - 1;
+  for (let i = 0; i < arr.length; i += 1)
+    if (arr[i].dataIndex === index) {
+      return arr[i].original;
+    }
+};
+
+function onPressKeydown(event) {
+  //общая функция для клавиатуры
+
   if (event.code === 'Escape') {
     closeModal();
   }
-}
+  if (event.code === 'ArrowRight') {
+    // if (counterValue >= galleryItems.length)
+    counterValue = counterValue + 1;
 
+    if (__index + 1 === galleryItems.length) {
+      closeModal();
+      __index = 1;
+      counterValue = 0;
+    }
+
+    return url(counterValue);
+  }
+  if (event.code === 'ArrowLeft') {
+    if (__index <= 0) {
+      closeModal();
+      __index = 1;
+      counterValue = 0;
+    }
+
+    // if (counterValue > 0)
+    counterValue = counterValue - 1;
+    return url(counterValue);
+  }
+}
 function closeModal(event) {
+  // снятие слушателя клавиатуры
   lightbox.classList.remove('is-open');
   img.src = '';
-  window.removeEventListener('keydown', onPressEsc);
+  window.removeEventListener('keydown', onPressKeydown);
 }
-
-// Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
